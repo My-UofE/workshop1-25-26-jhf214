6d745b2824e0db6edb5c516d37297eefd6628c1e
jhf214 <jhf214@exeter.ac.uk>
2026-01-21 19:40:55 +0000

    game 1 fix

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    mid_index = round(len(poss_values) / 2)
+    mid_index = len(poss_values) // 2
     return poss_values[mid_index]
 
 # function to be used in game_2: Higher or Lower
